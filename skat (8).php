0cf65b84722368e17526f4243986c0968e98feac
Jonas Bahceli <jonas@Jonas-Macbook-Pro.local>
2018-05-10 11:42:30 +0200

    prox1.0

---
+++ skat (8).php
@@ -0,0 +1,14 @@
+<?php
+
+
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update =file_get_contents($website."/getupdates");
+
+$updateArray =json_decode($update, TRUE);
+
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+
+?>
